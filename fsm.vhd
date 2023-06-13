25316f1065d404864f65e6a4566ae2f026198404
Aquila <97201817+aquila1004@users.noreply.github.com>
2023-06-13 17:50:10 -0300

    fix: fazendo lógica da fsm

--- fsm.vhd
+++ fsm.vhd
@@ -1,322 +1,8 @@
-
--- vsg_off
-library IEEE;
-use IEEE.std_logic_1164.all;
-
-entity dispenser1 is
-    port (
-        state: in std_logic;
-        dispenserSignal: out std_logic
-    );
-end entity dispenser1;
-
-architecture behavioral of dispenser1 is
-begin
-    process (state)
-    begin
-        if state = '1' then
-            dispenserSignal <= '1';
-        else
-            dispenserSignal <= '0';
-        end if;
-    end process;
-end architecture behavioral;
-
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity change_calculation1 is
-    port (
-        productPrice: in unsigned(7 downto 0);
-        coinSum: in unsigned(7 downto 0);
-        change: inout unsigned(7 downto 0);
-        segmentA: out std_logic;
-        segmentB: out std_logic;
-        segmentC: out std_logic;
-        segmentD: out std_logic;
-        segmentE: out std_logic;
-        segmentF: out std_logic;
-        segmentG: out std_logic
-    );
-end entity change_calculation1;
-
-architecture behavioral of change_calculation1 is
-begin
-    process (productPrice, coinSum)
-    begin
-        if coinSum >= productPrice then
-            change <= coinSum - productPrice;
-        else
-            change <= (others => '0');
-        end if;
-
-        case change is
-            when "00000000" =>
-                segmentA <= '0';
-                segmentB <= '1';
-                segmentC <= '1';
-                segmentD <= '1';
-                segmentE <= '1';
-                segmentF <= '1';
-                segmentG <= '1';
-            when "00000001" =>
-                segmentA <= '1';
-                segmentB <= '1';
-                segmentC <= '1';
-                segmentD <= '1';
-                segmentE <= '1';
-                segmentF <= '1';
-                segmentG <= '1';
-            when "00000010" =>
-                segmentA <= '1';
-                segmentB <= '0';
-                segmentC <= '0';
-                segmentD <= '0';
-                segmentE <= '0';
-                segmentF <= '0';
-                segmentG <= '0';
-            when "00000011" =>
-                segmentA <= '0';
-                segmentB <= '0';
-                segmentC <= '0';
-                segmentD <= '0';
-                segmentE <= '0';
-                segmentF <= '0';
-                segmentG <= '0';
-            when "00000100" =>
-                segmentA <= '0';
-                segmentB <= '0';
-                segmentC <= '0';
-                segmentD <= '0';
-                segmentE <= '0';
-                segmentF <= '0';
-                segmentG <= '0';
-            when "00000101" =>
-                segmentA <= '0';
-                segmentB <= '0';
-                segmentC <= '0';
-                segmentD <= '0';
-                segmentE <= '0';
-                segmentF <= '0';
-                segmentG <= '0';
-            when "00000110" =>
-                segmentA <= '0';
-                segmentB <= '0';
-                segmentC <= '0';
-                segmentD <= '0';
-                segmentE <= '0';
-                segmentF <= '0';
-                segmentG <= '0';
-            when "00000111" =>
-                segmentA <= '0';
-                segmentB <= '0';
-                segmentC <= '0';
-                segmentD <= '0';
-                segmentE <= '0';
-                segmentF <= '0';
-                segmentG <= '0';
-            when "00001000" =>
-                segmentA <= '0';
-                segmentB <= '0';
-                segmentC <= '0';
-                segmentD <= '0';
-                segmentE <= '0';
-                segmentF <= '0';
-                segmentG <= '0';
-            when others =>
-                -- Valor do troco maior que 9, definir todos os segmentos como inválidos
-                segmentA <= '0';
-                segmentB <= '0';
-                segmentC <= '0';
-                segmentD <= '0';
-                segmentE <= '0';
-                segmentF <= '0';
-                segmentG <= '0';
-        end case;
-    end process;
-end architecture behavioral;
-
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
-
-entity comparador1 is
-    generic (
-        NUM_COINS : integer := 6
-    );
-    port (
-        coinSum    : in  unsigned(NUM_COINS-1 downto 0);
-        productPrice: in  unsigned(NUM_COINS-1 downto 0);
-        dispense   : out std_logic
-    );
-end entity comparador1;
-
-architecture behavioral of comparador1 is
-begin
-    process (coinSum, productPrice)
-    begin
-        if coinSum >= productPrice then
-            dispense <= '1';
-        else
-            dispense <= '0';
-        end if;
-    end process;
-end architecture behavioral;
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-entity divisor_clock1 is
-port ( clk50MHz : in std_logic;
-       reset : in std_logic;
-       clk1Hz : out std_logic
-     );
-end divisor_clock1;
-
-architecture Behavioral of divisor_clock1 is
-
---signal count : integer := 0;
-signal b : std_logic := '0';
-begin
-
- -- clk generation. For 50MHz clock this process generates 1Hz clock.
-process(clk50MHz, b)
-	variable cnt : integer range 0 to 2**26-1;
-	begin
-		if(rising_edge(clk50MHz)) then
-		   if(reset = '1') then
-			   cnt := 0;
-			else
-			   cnt := cnt + 1;
-         end if;
-			if(cnt = 24999999) then
-				b <= not b;
-				cnt := 0;
-			end if;
-		end if;
-		clk1Hz <= b;
-	end process;
-end;
-
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
-
-entity registrador1 is
-    generic (
-        DATA_WIDTH: natural := 8
-    );
-    port (
-        coin: in unsigned(DATA_WIDTH-1 downto 0);
-        productPrice: in unsigned(DATA_WIDTH-1 downto 0);
-        clock: in std_logic;
-        reset: in std_logic;
-        sum: out unsigned(DATA_WIDTH-1 downto 0);
-        price: out unsigned(DATA_WIDTH-1 downto 0)
-    );
-end entity registrador1;
-
-architecture behavioral of registrador1 is
-    signal sum_reg: unsigned(DATA_WIDTH-1 downto 0);
-    signal price_reg: unsigned(DATA_WIDTH-1 downto 0);
-begin
-    process (clock, reset)
-    begin
-        if reset = '1' then
-            sum_reg <= (others => '0');
-            price_reg <= (others => '0');
-        elsif rising_edge(clock) then
-            sum_reg <= sum_reg + coin;
-            price_reg <= productPrice;
-        end if;
-    end process;
-
-    sum <= sum_reg;
-    price <= price_reg;
-end architecture behavioral;
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-entity coin_insertion1 is
-    port (
-        pino4: in std_logic;
-        pino5: in std_logic;
-        pino6: in std_logic;
-        pino7: in std_logic;
-        coinSum: inout unsigned(7 downto 0)
-    );
-end entity coin_insertion1;
-
-architecture behavioral of coin_insertion1 is
-    signal coins: unsigned(7 downto 0) := (others => '0');
-begin
-    process (pino4, pino5, pino6, pino7)
-    begin
-        if (pino4 /= '0') then
-            coins <= coins + 1;
-        end if;
-        
-        if (pino5 /= '0') then
-            coins <= coins + 2;
-        end if;
-        
-        if (pino6 /= '0') then
-            coins <= coins + 5;
-        end if;
-        
-        if (pino7 /= '0') then
-            coins <= coins + 10;
-        end if;
-        
-        -- Atribuição correta do valor de coins a coinSum
-        coinSum <= coins;
-    end process;
-end architecture behavioral;
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-entity product_selection1 is
-    port (
-        pino1: in std_logic;
-        pino2: in std_logic;
-        pino3: in std_logic;
-        produto_preco: out unsigned(7 downto 0)
-    );
-end entity product_selection1;
-
-architecture rtl of product_selection1 is
-begin
-    process (pino1, pino2, pino3)
-    begin
-        if (pino1 = '0' and pino2 = '0' and pino3 = '0') then
-            produto_preco <= to_unsigned(1, 8);  -- Produto 1
-        elsif (pino1 = '0' and pino2 = '0' and pino3 = '1') then
-            produto_preco <= to_unsigned(2, 8);  -- Produto 2
-        elsif (pino1 = '0' and pino2 = '1' and pino3 = '0') then
-            produto_preco <= to_unsigned(3, 8);  -- Produto 3
-        elsif (pino1 = '0' and pino2 = '1' and pino3 = '1') then
-            produto_preco <= to_unsigned(5, 8);  -- Produto 4
-        elsif (pino1 = '1' and pino2 = '0' and pino3 = '0') then
-            produto_preco <= to_unsigned(10, 8);  -- Produto 5
-        elsif (pino1 = '1' and pino2 = '0' and pino3 = '1') then
-            produto_preco <= to_unsigned(15, 8);  -- Produto 6
-        else
-            produto_preco <= (others => '0');
-        end if;
-    end process;
-end architecture rtl;
-
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
-
-entity fsm1 is 
+entity fsm is 
     port (
         pin01: in std_logic;
         pin02: in std_logic;
@@ -326,40 +12,40 @@ entity fsm1 is
         pin06: in std_logic;
         pin07: in std_logic;
         pin08: in std_logic; -- reset clock
-        pin09: in std_logic; -- reset clock
+        pin09: in std_logic; -- reset geral
         clk: in std_logic; -- clock
         change_display:  out std_logic;
         dispenser_display: out std_logic;
-        segmento_display0: out std_logic;
-        segmento_display1:out std_logic;
-        segmento_display2: out std_logic;
-        segmento_display3: out std_logic;
-        segmento_display4: out std_logic;
-        segmento_display5: out std_logic;
-        segmento_display6:out std_logic
-    );
-end fsm1;
+        segmento_display : std_logic_vector(0 to 6);
 
-architecture arch_fsm of fsm1 is
+        --Saida da fsm
+        s : out std_logic
+    );
+end fsm;
+
+architecture arch_fsm of fsm is
+
+    type state_type is (INIT, LOAD, ADD, CHANGE, DIS);
+    signal current_state, next_state : state_type;
 
     ---------------------------------
-	-- COMPONENTE SELETOR PRODUTOS --
-	---------------------------------
+  -- COMPONENTE SELETOR PRODUTOS --
+  ---------------------------------
 
-    component product_selection1 is
+    component product_selection is
         port (
             pino1: in std_logic;
             pino2: in std_logic;
             pino3: in std_logic;
             produto_preco: out unsigned(7 downto 0)
         );
-    end component product_selection1;
+    end component product_selection;
 
     -----------------------------------
-	-- COMPONENTE INSERTOR DE MOEDAS --
-	-----------------------------------
+  -- COMPONENTE INSERTOR DE MOEDAS --
+  -----------------------------------
 
-    component coin_insertion1 is
+    component coin_insertion is
         port (
             pino4: in std_logic;
             pino5: in std_logic;
@@ -367,13 +53,13 @@ architecture arch_fsm of fsm1 is
             pino7: in std_logic;
             coinSum: inout unsigned(7 downto 0)
         );
-    end component coin_insertion1;
+    end component coin_insertion;
 
     ----------------------------    
-	-- COMPONENTE REGISTRADOR --
-	----------------------------
+  -- COMPONENTE REGISTRADOR --
+  ----------------------------
     
-    component registrador1 is
+    component registrador is
         generic (
             DATA_WIDTH: natural := 8
         );
@@ -385,22 +71,22 @@ architecture arch_fsm of fsm1 is
             sum: out unsigned(DATA_WIDTH-1 downto 0);
             price: out unsigned(DATA_WIDTH-1 downto 0)
         );
-    end component registrador1;
+    end component registrador;
 
     ----------------------  
-	-- COMPONENTE CLOCK --
-	----------------------
-    component divisor_clock1 is
+  -- COMPONENTE CLOCK --
+  ----------------------
+    component divisor_clock is
         port ( clk50MHz : in std_logic;
                reset : in std_logic;
                clk1Hz : out std_logic
              );
-    end component divisor_clock1;
+    end component divisor_clock;
 
     ---------------------------  
-	-- COMPONENTE COMPARADOR --
-	---------------------------
-    component comparador1 is
+  -- COMPONENTE COMPARADOR --
+  ---------------------------
+    component comparador is
         generic (
             NUM_COINS : integer := 8
         );
@@ -409,40 +95,31 @@ architecture arch_fsm of fsm1 is
             productPrice: in  unsigned(NUM_COINS-1 downto 0);
             dispense   : out std_logic
         );
-    end component comparador1;
+    end component comparador;
 
     ----------------------  
-	-- COMPONENTE TROCO --
-	----------------------
-    component change_calculation1 is
+  -- COMPONENTE TROCO --
+  ----------------------
+    component change_calculation is
         port (
-            productPrice: in unsigned(7 downto 0)	;
+            productPrice: in unsigned(7 downto 0)  ;
             coinSum: in unsigned(7 downto 0);
             change: inout unsigned(7 downto 0);
-            segmentA: out std_logic;
-            segmentB: out std_logic;
-            segmentC: out std_logic;
-            segmentD: out std_logic;
-            segmentE: out std_logic;
-            segmentF: out std_logic;
-            segmentG: out std_logic
+            segment : out std_logic_vector(0 to 6)
         );
-    end component change_calculation1;
+    end component change_calculation;
 
     --------------------------  
-	-- COMPONENTE DISPENSER --
-	--------------------------
+  -- COMPONENTE DISPENSER --
+  --------------------------
     
-    component dispenser1 is
+    component dispenser is
         port(
             state: in std_logic;
             dispenserSignal: out std_logic
         );
 
-    end component dispenser1;
-
-
-
+    end component dispenser;
 
     signal saidaProdutoSelecionado : unsigned(7 downto 0); 
     signal precoProdutoSelecionado : unsigned(7 downto 0);
@@ -458,23 +135,22 @@ architecture arch_fsm of fsm1 is
 begin
 
     --------------------------
-	-- PORT MAP COMPONENTES --
-	--------------------------
+  -- PORT MAP COMPONENTES --
+  --------------------------
 
-    MeuClock: divisor_clock1 port map(
+    MeuClock: divisor_clock port map(
         clk50MHz => clk,
         reset => pin08,
         clk1Hz => saidaClock
     );
-
-    SelectProduct: product_selection1 port map(
+SelectProduct: product_selection port map(
         pino1 => pin01,
         pino2 => pin02,
         pino3 => pin03,
         produto_preco => saidaProdutoSelecionado
     );
 
-    InsertCoin: coin_insertion1 port map(
+    InsertCoin: coin_insertion port map(
         pino4 => pin04,
         pino5 => pin05,
         pino6 => pin06,
@@ -482,7 +158,7 @@ begin
         coinSum => somaMoedas
     );
 
-    MeuRegistrador: registrador1 port map(
+    MeuRegistrador: registrador port map(
         coin => somaMoedas,
         productPrice => saidaProdutoSelecionado,
         clock => clk,
@@ -491,36 +167,93 @@ begin
         price => precoRegistrador
     );
 
-    VerificadorDeTroco: change_calculation1 port map(
+    VerificadorDeTroco: change_calculation port map(
         productPrice => saidaProdutoSelecionado,
         coinSum => somaMoedas,
         change => troco,
-        segmentA => segmento_display0,
-        segmentB => segmento_display1,
-        segmentC => segmento_display2,
-        segmentD => segmento_display3,
-        segmentE => segmento_display4,
-        segmentF => segmento_display5,
-        segmentG => segmento_display6
+        segment => segmento_display  
     );
 
-    MeuComparador: comparador1 port map(
+    MeuComparador: comparador port map(
         coinSum => somaMoedas,
         productPrice => saidaProdutoSelecionado,
         dispense => saidaComparador
     );
 
-    MeuDispenser: dispenser1 port map(
+    MeuDispenser: dispenser port map(
         state => DispenserActive,
         dispenserSignal => dispenser_display
     );
 
     ------------------------------
-	-- FIM PORT MAP COMPONENTES --
-	------------------------------
+  -- FIM PORT MAP COMPONENTES --
+  ------------------------------
 
-    process(saidaClock)
+    processo_sequencial: process(clk, pin09)
+    begin
+        if(pin09 = '1') then
+            current_state <= INIT;
+
+        elsif(rising_edge(clk)) then
+            current_state <= next_state;
+        end if;
+    end process;
+
+    processo_combinacional: process(current_state)
+    begin
+        saidaProdutoSelecionado <= "00000000";
+        precoProdutoSelecionado <= "00000000";
+        somaMoedas              <= "00000000";
+        somaRegistrador         <= "00000000";
+        precoRegistrador        <= "00000000";
+        troco                   <= "00000000";
+        s                       <= '0';
+        
+        case current_state is
+            when INIT =>
+                
+                dispenser_display <= '0';
+                next_state <= LOAD;
+            
+            when LOAD =>
+                if(pin04 = '1' or pin05 = '1' or pin06 = '1' or pin07 = '1') then -- Se alguma moeda for inserida, vai pro estado ADD
+                    next_state <= ADD;
+
+                elsif(somaMoedas < precoRegistrador) then
+                    next_state <= LOAD;
+                
+                elsif(somaMoedas >= precoRegistrador) then
+                    next_state <= CHANGE;
+                end if;
+            
+            when ADD => -- Nesse estado é somado o valor das moedas inseridas com o preço que está no registrador
+                if(pin04 = '1') then
+                    precoRegistrador <= precoRegistrador + "00000001"; -- Valor 1 da moeda
+                
+                elsif(pin05 = '1') then
+                    precoRegistrador <= precoRegistrador + "00000010"; -- Valor 2 da moeda
+                
+                elsif(pin06 = '1') then
+                    precoRegistrador <= precoRegistrador + "00000101"; -- Valor 5 da moeda
+                
+                elsif(pin07 = '1') then
+                    precoRegistrador <= precoRegistrador + "00001010"; -- Valor 10 da moeda
+                
+                next_state <= LOAD;
+                end if;
+            
+            when CHANGE => -- Nesse estado é calculado o valor do troco
+                troco <= somaMoedas - precoRegistrador;
+                next_state <= DIS;
+            
+            when DIS =>
+                    s <= '1';
+                    current_state <= INIT;
+            end case;
+        end process;
+process(saidaClock)
         variable cnt : integer range 0 to 2**26-1;
+        variable teste : unsigned (7 downto 0) := "00000000";
         begin
             if(rising_edge(saidaClock)) then
                 if(pin08 = '1') then
@@ -530,7 +263,7 @@ begin
                 end if;
             end if;
         
-        if(troco /= "0000000") then
+        if(troco /= teste) then
             DispenserActive <= '1';
             if (cnt = 5) then
                 DispenserActive <= '0';
