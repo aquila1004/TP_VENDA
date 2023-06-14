9fe5f10a919ea55aff20f972678b5833b3b86da6
Áquila <97201817+aquila1004@users.noreply.github.com>
2023-06-13 21:48:27 -0300

    Update fsm.vhd

--- TP_VENDA-main/fsm.vhd
+++ TP_VENDA-main/fsm.vhd
@@ -1,166 +1,158 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
 
-entity fsm is 
-    port (
-        pin01: in std_logic;
-        pin02: in std_logic;
-        pin03: in std_logic;
-        pin04: in std_logic;
-        pin05: in std_logic;
-        pin06: in std_logic;
-        pin07: in std_logic;
-        pin08: in std_logic; -- reset clock
-        pin09: in std_logic; -- reset geral
-        clk: in std_logic; -- clock
-        change_display:  out std_logic;
-        dispenser_display: out std_logic;
-        segmento_display0: out std_logic;
-        segmento_display1:out std_logic;
-        segmento_display2: out std_logic;
-        segmento_display3: out std_logic;
-        segmento_display4: out std_logic;
-        segmento_display5: out std_logic;
-        segmento_display6:out std_logic
+ENTITY fsm IS
+    PORT (
+        pin01 : IN STD_LOGIC;
+        pin02 : IN STD_LOGIC;
+        pin03 : IN STD_LOGIC;
+        pin04 : IN STD_LOGIC;
+        pin05 : IN STD_LOGIC;
+        pin06 : IN STD_LOGIC;
+        pin07 : IN STD_LOGIC;
+        pin08 : IN STD_LOGIC; -- reset clock
+        pin09 : IN STD_LOGIC; -- reset geral
+        clk : IN STD_LOGIC; -- clock
+        enter : IN STD_LOGIC; -- enter
+        change_display : OUT STD_LOGIC;
+        dispenser_display : OUT STD_LOGIC;
+        segmento_display : out STD_LOGIC_VECTOR(0 TO 6);
+
+        --Saida da fsm
+        s : OUT STD_LOGIC
     );
-end fsm;
+END fsm;
 
-architecture arch_fsm of fsm is
+ARCHITECTURE arch_fsm OF fsm IS
 
-    type state_type is (INIT, WAIT_COIN, ADD, CHANGE, DIS);
-    signal current_state, next_state : state_type;
+    TYPE state_type IS (INIT, SELECT1, LOAD, ADD, CHANGE, DIS);
+    SIGNAL current_state, next_state : state_type;
 
     ---------------------------------
-	-- COMPONENTE SELETOR PRODUTOS --
-	---------------------------------
+    -- COMPONENTE SELETOR PRODUTOS --
+    ---------------------------------
 
-    component product_selection is
-        port (
-            pino1: in std_logic;
-            pino2: in std_logic;
-            pino3: in std_logic;
-            produto_preco: out unsigned(7 downto 0)
+    COMPONENT product_selection IS
+        PORT (
+            pino1 : IN STD_LOGIC;
+            pino2 : IN STD_LOGIC;
+            pino3 : IN STD_LOGIC;
+            produto_preco : OUT unsigned(7 DOWNTO 0)
         );
-    end component product_selection;
+    END COMPONENT product_selection;
 
     -----------------------------------
-	-- COMPONENTE INSERTOR DE MOEDAS --
-	-----------------------------------
+    -- COMPONENTE INSERTOR DE MOEDAS --
+    -----------------------------------
 
-    component coin_insertion is
-        port (
-            pino4: in std_logic;
-            pino5: in std_logic;
-            pino6: in std_logic;
-            pino7: in std_logic;
-            coinSum: inout unsigned(7 downto 0)
+    COMPONENT coin_insertion IS
+        PORT (
+            pino4 : IN STD_LOGIC;
+            pino5 : IN STD_LOGIC;
+            pino6 : IN STD_LOGIC;
+            pino7 : IN STD_LOGIC;
+            coinSum : INOUT unsigned(7 DOWNTO 0)
         );
-    end component coin_insertion;
+    END COMPONENT coin_insertion;
 
     ----------------------------    
-	-- COMPONENTE REGISTRADOR --
-	----------------------------
-    
-    component registrador is
-        generic (
-            DATA_WIDTH: natural := 8
+    -- COMPONENTE REGISTRADOR --
+    ----------------------------
+
+    COMPONENT registrador IS
+        GENERIC (
+            DATA_WIDTH : NATURAL := 8
         );
-        port (
-            coin: in unsigned(DATA_WIDTH-1 downto 0);
-            productPrice: in unsigned(DATA_WIDTH-1 downto 0);
-            clock: in std_logic;
-            reset: in std_logic;
-            sum: out unsigned(DATA_WIDTH-1 downto 0);
-            price: out unsigned(DATA_WIDTH-1 downto 0)
+        PORT (
+            coin : IN unsigned(DATA_WIDTH - 1 DOWNTO 0);
+            productPrice : IN unsigned(DATA_WIDTH - 1 DOWNTO 0);
+            clock : IN STD_LOGIC;
+            reset : IN STD_LOGIC;
+            sum : OUT unsigned(DATA_WIDTH - 1 DOWNTO 0);
+            price : OUT unsigned(DATA_WIDTH - 1 DOWNTO 0)
         );
-    end component registrador;
+    END COMPONENT registrador;
 
     ----------------------  
-	-- COMPONENTE CLOCK --
-	----------------------
-    component divisor_clock is
-        port ( clk50MHz : in std_logic;
-               reset : in std_logic;
-               clk1Hz : out std_logic
-             );
-    end component divisor_clock;
+    -- COMPONENTE CLOCK --
+    ----------------------
+    COMPONENT divisor_clock IS
+        PORT (
+            clk50MHz : IN STD_LOGIC;
+            reset : IN STD_LOGIC;
+            clk1Hz : OUT STD_LOGIC
+        );
+    END COMPONENT divisor_clock;
 
     ---------------------------  
-	-- COMPONENTE COMPARADOR --
-	---------------------------
-    component comparador is
-        generic (
-            NUM_COINS : integer := 8
+    -- COMPONENTE COMPARADOR --
+    ---------------------------
+    COMPONENT comparador IS
+        GENERIC (
+            NUM_COINS : INTEGER := 8
         );
-        port (
-            coinSum    : in  unsigned(NUM_COINS-1 downto 0);
-            productPrice: in  unsigned(NUM_COINS-1 downto 0);
-            dispense   : out std_logic
+        PORT (
+            coinSum : IN unsigned(NUM_COINS - 1 DOWNTO 0);
+            productPrice : IN unsigned(NUM_COINS - 1 DOWNTO 0);
+            dispense : OUT STD_LOGIC
         );
-    end component comparador;
+    END COMPONENT comparador;
 
     ----------------------  
-	-- COMPONENTE TROCO --
-	----------------------
-    component change_calculation is
-        port (
-            productPrice: in unsigned(7 downto 0)	;
-            coinSum: in unsigned(7 downto 0);
-            change: inout unsigned(7 downto 0);
-            segmentA: out std_logic;
-            segmentB: out std_logic;
-            segmentC: out std_logic;
-            segmentD: out std_logic;
-            segmentE: out std_logic;
-            segmentF: out std_logic;
-            segmentG: out std_logic
+    -- COMPONENTE TROCO --
+    ----------------------
+    COMPONENT change_calculation IS
+        PORT (
+            productPrice : IN unsigned(7 DOWNTO 0);
+            coinSum : IN unsigned(7 DOWNTO 0);
+            change : INOUT unsigned(7 DOWNTO 0);
+            segment : OUT STD_LOGIC_VECTOR(0 TO 6)
         );
-    end component change_calculation;
+    END COMPONENT change_calculation;
 
     --------------------------  
-	-- COMPONENTE DISPENSER --
-	--------------------------
-    
-    component dispenser is
-        port(
-            state: in std_logic;
-            dispenserSignal: out std_logic
+    -- COMPONENTE DISPENSER --
+    --------------------------
+
+    COMPONENT dispenser IS
+        PORT (
+            state : IN STD_LOGIC;
+            dispenserSignal : OUT STD_LOGIC
         );
 
-    end component dispenser;
+    END COMPONENT dispenser;
 
-    signal saidaProdutoSelecionado : unsigned(7 downto 0); 
-    signal precoProdutoSelecionado : unsigned(7 downto 0);
-    signal somaMoedas : unsigned(7 downto 0);
-    signal somaRegistrador : unsigned(7 downto 0);
-    signal precoRegistrador : unsigned(7 downto 0);
-    signal saidaClock : std_logic;
-    signal saidaComparador : std_logic;
-    signal troco : unsigned(7 downto 0);
-    signal MeuReset : std_logic;
-    signal DispenserActive : std_logic;
+    SIGNAL saidaProdutoSelecionado : unsigned(7 DOWNTO 0);
+    SIGNAL precoProdutoSelecionado : unsigned(7 DOWNTO 0);
+    SIGNAL somaMoedas : unsigned(7 DOWNTO 0);
+    SIGNAL somaRegistrador : unsigned(7 DOWNTO 0);
+    SIGNAL precoRegistrador : unsigned(7 DOWNTO 0);
+    SIGNAL saidaClock : STD_LOGIC;
+    SIGNAL saidaComparador : STD_LOGIC;
+    SIGNAL troco : unsigned(7 DOWNTO 0);
+    SIGNAL MeuReset : STD_LOGIC;
+    SIGNAL DispenserActive : STD_LOGIC;
 
-begin
+BEGIN
 
     --------------------------
-	-- PORT MAP COMPONENTES --
-	--------------------------
+    -- PORT MAP COMPONENTES --
+    --------------------------
 
-    MeuClock: divisor_clock port map(
+    MeuClock : divisor_clock PORT MAP(
         clk50MHz => clk,
         reset => pin08,
         clk1Hz => saidaClock
     );
-
-    SelectProduct: product_selection port map(
+    SelectProduct : product_selection PORT MAP(
         pino1 => pin01,
         pino2 => pin02,
         pino3 => pin03,
         produto_preco => saidaProdutoSelecionado
     );
 
-    InsertCoin: coin_insertion port map(
+    InsertCoin : coin_insertion PORT MAP(
         pino4 => pin04,
         pino5 => pin05,
         pino6 => pin06,
@@ -168,7 +160,7 @@ begin
         coinSum => somaMoedas
     );
 
-    MeuRegistrador: registrador port map(
+    MeuRegistrador : registrador PORT MAP(
         coin => somaMoedas,
         productPrice => saidaProdutoSelecionado,
         clock => clk,
@@ -177,63 +169,129 @@ begin
         price => precoRegistrador
     );
 
-    VerificadorDeTroco: change_calculation port map(
+    VerificadorDeTroco : change_calculation PORT MAP(
         productPrice => saidaProdutoSelecionado,
         coinSum => somaMoedas,
         change => troco,
-        segmentA => segmento_display0,
-        segmentB => segmento_display1,
-        segmentC => segmento_display2,
-        segmentD => segmento_display3,
-        segmentE => segmento_display4,
-        segmentF => segmento_display5,
-        segmentG => segmento_display6
+        segment => segmento_display
     );
 
-    MeuComparador: comparador port map(
+    MeuComparador : comparador PORT MAP(
         coinSum => somaMoedas,
         productPrice => saidaProdutoSelecionado,
         dispense => saidaComparador
     );
 
-    MeuDispenser: dispenser port map(
+    MeuDispenser : dispenser PORT MAP(
         state => DispenserActive,
         dispenserSignal => dispenser_display
     );
 
     ------------------------------
-	-- FIM PORT MAP COMPONENTES --
-	------------------------------
+    -- FIM PORT MAP COMPONENTES --
+    ------------------------------
 
-    processo_sequencial: process(clk, pin09)
-    begin
-        if(pin09 = '1') then
+    processo_sequencial : PROCESS (clk, pin09)
+    BEGIN
+        IF (pin09 = '1') THEN
             current_state <= INIT;
-        elsif(rising_edge(clk)) then
+
+        ELSIF (rising_edge(clk)) THEN
             current_state <= next_state;
-        end if;
-    end process;
+        END IF;
+    END PROCESS;
 
-    process(saidaClock)
-        variable cnt : integer range 0 to 2**26-1;
-        variable teste : unsigned (7 downto 0) := "00000000";
-        begin
-            if(rising_edge(saidaClock)) then
-                if(pin08 = '1') then
+    processo_combinacional : PROCESS (current_state)
+    BEGIN
+        precoProdutoSelecionado <= "00000000";
+        somaMoedas <= "00000000";
+        somaRegistrador <= "00000000";
+        precoRegistrador <= "00000000";
+        troco <= "00000000";
+        s <= '0';
+
+        CASE current_state IS
+            WHEN INIT =>
+                dispenser_display <= '0';
+                saidaProdutoSelecionado <= "00000000";
+                precoProdutoSelecionado <= "00000000";
+                somaMoedas <= "00000000";
+                somaRegistrador <= "00000000";
+                precoRegistrador <= "00000000";
+                troco <= "00000000";
+                next_state <= SELECT1;
+
+            WHEN SELECT1 =>
+                IF (pin01 = '0' AND pin02 = '0' AND pin03 = '0' and enter = '1') THEN
+                    next_state <= LOAD;
+                ELSIF (pin01 = '0' AND pin02 = '0' AND pin03 = '1' and enter = '1') THEN
+                    next_state <= LOAD;
+                ELSIF (pin01 = '0' AND pin02 = '1' AND pin03 = '0' and enter = '1') THEN
+                    next_state <= LOAD;
+                ELSIF (pin01 = '0' AND pin02 = '1' AND pin03 = '1' and enter = '1') THEN
+                    next_state <= LOAD;
+                ELSIF (pin01 = '1' AND pin02 = '0' AND pin03 = '0' and enter = '1') THEN
+                    next_state <= LOAD;
+                ELSIF (pin01 = '1' AND pin02 = '0' AND pin03 = '1' and enter = '1') THEN
+                    next_state <= LOAD;
+                    else 
+                    next_state <= SELECT1;
+					END IF;
+                WHEN LOAD =>
+                    IF (pin04 = '1' OR pin05 = '1' OR pin06 = '1' OR pin07 = '1') THEN -- Se alguma moeda for inserida, vai pro estado ADD
+                        next_state <= ADD;
+
+                    ELSIF (somaMoedas < precoRegistrador) THEN
+                        next_state <= LOAD;
+
+                    ELSIF (somaMoedas >= precoRegistrador) THEN
+                        next_state <= CHANGE;
+                    END IF;
+
+                WHEN ADD => -- Nesse estado é somado o valor das moedas inseridas com o preço que está no registrador
+                    IF (pin04 = '1') THEN
+                        precoRegistrador <= precoRegistrador + "00000001"; -- Valor 1 da moeda
+
+                    ELSIF (pin05 = '1') THEN
+                        precoRegistrador <= precoRegistrador + "00000010"; -- Valor 2 da moeda
+
+                    ELSIF (pin06 = '1') THEN
+                        precoRegistrador <= precoRegistrador + "00000101"; -- Valor 5 da moeda
+
+                    ELSIF (pin07 = '1') THEN
+                        precoRegistrador <= precoRegistrador + "00001010"; -- Valor 10 da moeda
+
+                        next_state <= LOAD;
+                    END IF;
+
+                WHEN CHANGE => -- Nesse estado é calculado o valor do troco
+                    troco <= somaMoedas - precoRegistrador;
+                    next_state <= DIS;
+
+                WHEN DIS =>
+                    s <= '1';
+                    current_state <= INIT;
+                END CASE;
+        END PROCESS;
+        PROCESS (saidaClock)
+            VARIABLE cnt : INTEGER RANGE 0 TO 2 ** 26 - 1;
+            VARIABLE teste : unsigned (7 DOWNTO 0) := "00000000";
+        BEGIN
+            IF (rising_edge(saidaClock)) THEN
+                IF (pin08 = '1') THEN
                     cnt := 0;
-                else
+                ELSE
                     cnt := cnt + 1;
-                end if;
-            end if;
-        
-        if(troco /= teste) then
-            DispenserActive <= '1';
-            if (cnt = 5) then
-                DispenserActive <= '0';
-                MeuReset <= '1';
-            end if;
-        end if;
-        MeuReset <= '0';
-    end process;
+                END IF;
+            END IF;
 
-end arch_fsm;
+            IF troco /= 'U' THEN
+                DispenserActive <= '1';
+                IF cnt = 5 THEN
+                    DispenserActive <= '0';
+                    MeuReset <= '1';
+                END IF;
+            END IF;
+        END PROCESS;
+
+    END arch_fsm;
